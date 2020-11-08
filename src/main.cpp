d0e288480b02049bcc416186205553285116c73d
Tachi <andreapappacoda02@gmail.com>
2020-11-08 16:03:27 +0100

    First time using Pistache; simple http server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <memory>
 #include <pistache/endpoint.h>
 
+using namespace Pistache;
+
+class HelloHandler : public Http::Handler {
+public:
+	// I should define the clone() function,
+	// but since this http handler is really simple I can use the Pistache macro that does that for me
+	HTTP_PROTOTYPE(HelloHandler)
+
+	void onRequest(const Http::Request& request, Http::ResponseWriter response) override {
+		response.send(Http::Code::Ok, "Luca suca");
+	}
+};
+
 int main() {
-	std::cout << "Luca suca\n";
+	Address address(Ipv4::any(), Port(10780));
+	auto httpOptions = Http::Endpoint::options().threads(1);
+	Http::Endpoint server(address);
+	server.init(httpOptions);
+	server.setHandler(std::make_shared<HelloHandler>());
+	server.serve();
+	// Or I can simply use Http::listenAndServe<HelloHandler>("*:10780", options) that sets up everything for me
 }
