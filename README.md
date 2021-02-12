85007948dfe91b7a4119a9f09b567ecc7bfae576
Tachi <andrea@pappacoda.it>
2021-02-13 00:24:46 +0100

    Scraf class skeleton Dependency injection, /students route, json parsing

--- README.md
+++ README.md
@@ -18,22 +18,39 @@ scraf-backend uses C++20 and requires a modern compiler. It should work on Clang
 
 [ODB](https://www.codesynthesis.com/products/odb/) is an Object-Relational Mapping (ORM) library.
 The stable version of ODB packaged by Debian does not support anything newer than C++14, so it is required to use the beta plus the module for PostgreSQL.
-It is recommended to follow the official [installation guide](https://www.codesynthesis.com/products/odb/doc/install-build2.xhtml#linux), but if you're lazy you can install it with this two commands (spoiler: it is goning to take more than 10 minutes, go drink a tea or something)
+It is recommended to follow the official [installation guide](https://www.codesynthesis.com/products/odb/doc/install-build2.xhtml#linux), but if you're lazy you can install it with these commands (spoiler: it is goning to take more than 10 minutes, go drink a tea or something)
 
 ```sh
-cd /directory/of/your/choice/where/you/ll/store/build/cache
-
-mkdir build2-build && cd build2-build && curl -sSfO https://download.build2.org/0.13.0/build2-install-0.13.0.sh && sh build2-install-0.13.0.sh --yes --no-check --trust yes
+sudo apt install gcc-10-plugin-dev
 
 cd /directory/of/your/choice/where/you/ll/store/build/cache
 
-mkdir odb-build && cd odb-build && bpkg create --directory odb-gcc-10 cc config.cxx=g++ config.cc.coptions=-O3 config.bin.rpath=/usr/local/lib config.install.root=/usr/local config.install.sudo=sudo --trust-yes && cd odb-gcc-10 && (yes | bpkg build odb@https://pkg.cppget.org/1/beta --trust-yes) && bpkg test odb && bpkg install odb --trust-yes && cd .. && bpkg create --directory gcc-10 cc config.cxx=g++ config.cc.coptions=-O3 config.install.root=/usr/local config.install.sudo=sudo && cd gcc-10 && bpkg add https://pkg.cppget.org/1/beta --trust-yes && (yes | bpkg fetch --trust-yes) && (yes | bpkg build libodb --trust-yes) && (yes | bpkg build libodb-pgsql --trust-yes) && (yes | bpkg install --trust-yes --all --recursive)
+mkdir build2-build && cd build2-build
+curl -sSfO https://download.build2.org/0.13.0/build2-install-0.13.0.sh
+sh build2-install-0.13.0.sh --yes --no-check --trust yes ~/build2
+
+cd /directory/of/your/choice/where/you/ll/store/build/cache
+
+mkdir odb-build && cd odb-build
+bpkg create --directory odb-gcc-10 cc config.cxx=g++ config.cc.coptions=-O3 config.bin.rpath=/usr/local/lib config.install.root=/usr/local config.install.sudo=sudo --quiet
+cd odb-gcc-10
+yes | bpkg build odb@https://pkg.cppget.org/1/beta --trust-yes --quiet
+bpkg test odb
+bpkg install odb --quiet
+cd ..
+bpkg create --directory gcc-10 cc config.cxx=g++ config.cc.coptions=-O3 config.install.root=/usr/local config.install.sudo=sudo --quiet
+cd gcc-10
+bpkg add https://pkg.cppget.org/1/beta --trust-yes --quiet
+bpkg fetch --trust-yes --quiet
+yes | bpkg build libodb --trust-yes --quiet
+yes | bpkg build libodb-pgsql --trust-yes --quiet
+bpkg install --all --recursive --quiet
 ```
 
 ##### GoogleTest
 
-We use [GoogleTest](https://github.com/google/googletest) for unit testing. We chose not to include the library as a submodule of scraf-backend because it is already packaged by most Linux distributions. We use the latest version provided by Debian Testing, but probably anything newer than 1.9.x will work. On Debian you can install it with
+We use [GoogleTest](https://github.com/google/googletest) for unit testing. We chose not to include the library as a submodule of scraf-backend because it is already packaged by most Linux distributions. We use the latest version provided by Debian Testing, but probably anything newer than 1.9.x will work. On Debian you can install it with `apt install libgtest-dev`
 
-```sh
-sudo apt install libgtest-dev
-```
+##### JSON
+
+For parsing and serializing JSON we use [simdjson](https://github.com/simdjson/simdjson) and [nlohmann json](https://github.com/nlohmann/json) (also known as JSON for Modern C++ or the cool JSON library). On a Debian system, you can install them with `apt install libsimdjson-dev nlohmann-json3-dev`.
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
+/*
+* TDD - Test Driven Development
+* https://www.youtube.com/watch?v=RoYljVOj2H8
+* 
+* Andrò ad usare la Constructor Injection,
+* un tipo di Dependency Injection,
+* che è un caso particolare di Inversion of Control.
+* In pratica, la mia classe da testare non ha più
+* un controllo completo dei suoi attributi, o meglio,
+* io nella classe Scraf scrivo per esempio che
+* essa ha un attributo "database" di tipo "Database",
+* ma faccio sì che io al posto di un oggetto Database
+* gli possa passare un oggetto TestDatabase, che
+* assomiglia a un Database ma che viene usato solo per i test,
+* senza che la classe si accorga della differenza.
+*/
+
 #include <iostream>
 #include <memory>
+#include <utility>
 #include <csignal>
 
 #include <odb/database.hxx>
@@ -11,21 +29,34 @@
 #include "student_odb.hpp"
 
 #include <pistache/endpoint.h>
+#include <pistache/router.h>
+
+#include <simdjson.h>
 
 using namespace Pistache;
 
-class HelloHandler : public Http::Handler {
+template<typename Database = odb::database, typename Endpoint = Http::Endpoint>
+class Scraf {
 public:
-	// I should define the clone() function,
-	// but since this http handler is really simple I can use the Pistache macro that does that for me
-	HTTP_PROTOTYPE(HelloHandler)
-
-	void onRequest([[maybe_unused]] const Http::Request& request, Http::ResponseWriter response) override {
-		//db_connection.perform([&response](auto&& row) {
-		//	auto schoolCode {pgfe::to<std::string>(row[0])};
-		//	response.send(Http::Code::Ok, "La tua scuola è " + schoolCode + '?');
-		//}, "SELECT code FROM school WHERE name LIKE '%Cobianchi%'");
+	Scraf(Database& database, Endpoint& endpoint)
+		: database(database), endpoint(endpoint) {
+		endpoint.init();
+		Rest::Routes::Post(router, "/students", Rest::Routes::bind(&Scraf::createStudent, this));
+		endpoint.setHandler(router.handler());
+		endpoint.serve();
 	}
+
+private:
+	void createStudent(const Rest::Request& request, Http::ResponseWriter response) {
+		parser.parse(request.body());
+		std::cerr << request.body();
+	}
+private:
+	Database& database;
+	Endpoint& endpoint;
+	Rest::Router router;
+	// NON È THREAD-SAFE
+	simdjson::dom::parser parser;
 };
 
 void handleSignals() {
@@ -39,131 +70,8 @@ void handleSignals() {
 }
 
 int main() {
-	std::unique_ptr<odb::database> database(new odb::pgsql::database("scraf", "ollareollare", "scraf_prova", "postgres.barto.paoloap.ml", 5432));
-	{
-		// Creo la transazione, ovvero l'oggetto che si occuperà di inviare i cambiamenti al database
-		odb::transaction transaction(database->begin());
-		// I controlli con # vengono eseguiti prima della compilazione, e mi permettono di compilare del codice
-		// in base a cose come se sto compilando il programma in Debug o Release;
-		// se l'#if è false, il codice al suo interno viene ignorato.
-		// In questo caso, sto dicendo "se NDEBUG non è definito, abilita il tracciamento dell'SQL";
-		// NDEBUG viene definito automaticamente quando sto compilando in Release,
-		// quindi questo codice sarà presente solo in non non-debug, quindi debug.
-		#ifndef NDEBUG
-		transaction.tracer(odb::stderr_tracer);
-		#endif
-
-		// Voglio stampare a schermo il codice della scuola il cui nome equivale a "IS L. Cobianchi".
-		// (Da destra a sinistra) faccio una query al database e gli chiedo di restituirmi tutte le scuole
-		// il cui nome sia uguale a "IS L. Cobianchi", e mi salvo il risultato della query in una variabile result.
-		// Il tipo di result dev'essere un odb::result dell'oggetto che sto cercando, in questo caso school,
-		// e così anche la query, che è una query di tipo school.
-		odb::result<school> result {database->query<school>(odb::query<school>::name == "IS L. Cobianchi")};
-		// Una volta aver ottenuto il risultato non posso stamparlo, perché non ho la certezza di aver ottenuto un singolo
-		// oggetto school, in quando name non è UNIQUE o PRIMARY KEY. 
-		// Per questo motivo, odb::result è una specie di array.
-		// Per iterare attraverso l'array dei risultati uso un range based for loop, ovvero un for che itera su un range;
-		// quindi quello che faccio è creare un for che per ad ogni iterazione su result crea un oggetto chiamato school,
-		// e dato che il tipo di school non mi interessa metto auto e lascio che sia il compilatore a mettere il tipo al posto mio.
-		// In più metto una & a fianco al tipo così che non venga effettivamente creato un oggetto ad ogni iterazione,
-		// ma che l'oggetto sia un riferimento a quello contenuto in result, evitando copie inutili,
-		// e metto const perché non ho bisogno di modificare school (è raccomandato mettere sempre const se possibile).
-		// Infine, faccio l'output di del code di ogni oggetto school in result, e in questo esempio so già che in realtà ce n'è solo 1.
-		for (const auto& school : result) {
-			std::cout << "Il codice del Cobianchi è " << school.code << '\n';
-		}
-
-		// Credo che questo sia il modo in cui bisogna lavorare con l'ODB. 
-		// Per lavorare su un oggetto mi serve il suo id.
-		// Per poter ottenere l'id devo fare un paio di controlli;
-		// se l'oggetto sul quale voglio lavorare esiste già nel database dovrò prendere l'id già esistente,
-		// se invece l'oggetto non esiste devo crearlo e poi usare l'id assegnato.
-		// L'id non dev'essere modificato dal mio programma (una chiave primaria è immutabile)
-		// quindi lo dichiaro const. Dichiarandolo const però ho un problema:
-		// come faccio a sapere se l'oggetto esiste già nel db prima ancora di crearlo?
-		// Normalmente creerei studentPappacodaId vuoto, faccio tutti i controlli
-		// e poi assegno il valore corretto alla variabile, ma facendo così dovrei dire addio a const.
-		// Fortunatamente in C++ esistono le lambda, ovvero funzioni usa e getta che avevo già spiegato
-		// in un commit che mannaggia a git non so dove caz sia finito... Vabbè, tanto non ho niente da fare, vero Fauster?
-		//
-		// Una lambda è una funzione usa e getta che posso dichiarare così:
-		// [*captures*](*parameters*) {*body*}
-		//
-		// Le captures sono le variabili che la lambda può "catturare" dall'esterno;
-		// in questo caso metto &, che sta a significare che la lambda può accedere a tutte le variabili esterne
-		// per riferimento, quindi senza copie inutili e potenzialmente dannose. 
-		// Se volessi dire esplicitamente che la lambda può accedere solamente alla variabile database
-		// scriverei [&database].
-		//
-		// In parameters posso passare dei parametri come farei normalmente con una funzione,
-		// che boh ok va bene si può fare ma a me non è mai servito
-		// perché sveglia C++ ci sono già le captures che fanno la stessa indentica fucking cosa, quindi lascio vuoto.
-		//
-		// Infine in body scrivo il corpo della lamda, ez.
-		// In questo caso nel corpo ci scrivo l'if che controlla se l'oggetto esiste già e agisce di conseguenza,
-		// così che posso ritornare l'id direttamente durante l'inizializzazione e non dire addio al bellissimo const.
-		// Ah, non so se l'ho già detto ma in C++ ci sono 48173 modi diversi per inizializzare una variabile, tipo:
-		// std::string paolo("nazionale");
-		// std::string paolo = "nazionale";
-		// std::string paolo = std::string("nazionale");
-		// std::string paolo = {"nazionale"};
-		// std::string paolo {"nazionale"};
-		// Per una serie di motivi che non ho voglia di spiegare la migliore inizializzazione è l'ultima,
-		// quella con {"..."} e senza l'uguale. Quindi abituati a usare quella. Sempre. Solo per l'inizializzazione.
-		// Anche il classicissimo for va scritto con {}, quindi sarebbe
-		// for (int i {0}; i < nv; ++i) {
-		//     ...
-		// }
-		// So che viene naturale scrivere int i = 0, ma è meglio usare {0}.
-		const std::int64_t studentPappacodaId {[&]() {
-			std::unique_ptr<student> studentResult {database->query_one<student>(odb::query<student>::mail == "andrea@pappacoda.it")};
-			if (!studentResult) {
-				student pappacoda {"andrea@pappacoda.it", "Andrea", "Pappacoda"};
-				return database->persist(pappacoda);
-			}
-			else {
-				return studentResult->getId();
-			}
-		}()};
-
-		std::cerr << "L'id dello studente con la mail andrea@pappacoda.it è " << studentPappacodaId << '\n';
-
-		// In pratica preparo prima una serie di robe da fare, ma non le faccio,
-		// e poi con transaction.commit() le faccio effettivamente.
-		// Questo mi permette di fare più cose in una sola volta, migliorando le prestazioni.
-		transaction.commit();
-
-
-
-		// Aggiornare un oggetto conoscendo già l'id
-		{
-			odb::transaction transactionn {database->begin()};
-			std::unique_ptr<student> studentPappacoda {database->load<student>(studentPappacodaId)};
-			studentPappacoda->name = "Tachi";
-			database->update(*studentPappacoda);
-			std::cout << "Ora il nome dello studente con id " << studentPappacoda->getId() << " è \"" << studentPappacoda->name.get() << "\"\n";
-			transactionn.commit();
-		}
-
-
-
-		// Aggiornare un oggetto solo senza conoscere l'id
-		{
-			odb::transaction transactionn {database->begin()};
-			std::unique_ptr<student> studentPappacoda {database->query_one<student>(odb::query<student>::mail == "andrea@pappacoda.it")};
-			if (studentPappacoda) {
-				studentPappacoda->name = "Andrea";
-				database->update(*studentPappacoda);
-				std::cout << "Ora il nome dello studente con id " << studentPappacoda->getId() << " è \"" << studentPappacoda->name.get() << "\"\n";
-			}
-			else {
-				std::cout << "Non esiste nessuno studente con la mail \"andrea@pappacoda.it\"\n";
-			}
-			transactionn.commit();
-		}
-	}
-
+	std::unique_ptr<odb::database> database {new odb::pgsql::database{"scraf", "ollareollare", "scraf_prova", "postgres.barto.paoloap.ml", 5432}};
+	Http::Endpoint endpoint{{Ipv4::any(), Port(10780)}};
 	handleSignals();
-
-	Http::listenAndServe<HelloHandler>(Address(Ipv4::any(), Port(10780)), Http::Endpoint::options().threads(1));
+	Scraf scraf {database, endpoint};
 }
