b0c7609fc709f5fd26ec1f79d1aca94b40344206
Tachi <andrea@pappacoda.it>
2021-02-13 23:48:52 +0100

    Add libsodium for password hashing

--- src/student.hpp
+++ src/student.hpp
@@ -11,8 +11,9 @@ class student {
 public:
 	#pragma db unique
 	std::string mail;
+	std::string password_hash;
+	std::string name;
 	// Per esprimere che un oggetto può essere NULL nel database, lo dichiaro odb::nullable (o forse in futuro std::optional)
-	odb::nullable<std::string> name;
 	odb::nullable<std::string> surname;
 
 	template<Text type>
@@ -20,12 +21,16 @@ public:
 		: mail(std::forward<type>(mail)) {}
 	
 	template<Text type1, Text type2>
-	student(type1 mail, type2 name)
-		: mail(std::forward<type1>(mail)), name(std::forward<type2>(name)) {}
+	student(type1 mail, type2 password_hash)
+		: mail(std::forward<type1>(mail)), password_hash(std::forward<type2>(password_hash)) {}
 	
 	template<Text type1, Text type2, Text type3>
-	student(type1 mail, type2 name, type3 surname)
-		: mail(std::forward<type1>(mail)), name(std::forward<type2>(name)), surname(std::forward<type3>(surname)) {}
+	student(type1 mail, type2 password_hash, type3 name)
+		: mail(std::forward<type1>(mail)), password_hash(std::forward<type2>(password_hash)), name(std::forward<type3>(name)) {}
+	
+	template<Text type1, Text type2, Text type3, Text type4>
+	student(type1 mail, type2 password_hash, type3 name, type4 surname)
+		: mail(std::forward<type1>(mail)), password_hash(std::forward<type2>(password_hash)), name(std::forward<type3>(name)), surname(std::forward<type4>(surname)) {}
 
 	//template<Text... type>
 	//student(type... arguments)
