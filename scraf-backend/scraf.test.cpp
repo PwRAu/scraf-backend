49b3048d83811657bfe46f45e27b47bd4ef678ec
LucaLettini <luca.lettini@libero.it>
2021-03-06 16:07:46 +0100

    Testing session whitout real test(ne prende 1)

--- scraf-backend/scraf.test.cpp
+++ scraf-backend/scraf.test.cpp
@@ -48,3 +48,101 @@ TEST(ScrafTest, MailPasswordNameSurname) {
 	scraf.shutdown();
 	servingThread.join();
 }
+
+
+//test in cui la richiesta è effettuata senza il cognome = dovrebbe andare a buon fine
+TEST(ScrafTest, MailPasswordNameWhithoutSurname) {
+	constexpr std::uint16_t port {10780};
+	std::unique_ptr<FakeDatabase> database {std::make_unique<FakeDatabase>()};
+	Http::Endpoint endpoint{{Ipv4::loopback(), Port(port)}};
+	Scraf<std::unique_ptr<FakeDatabase>, FakeDbTransaction> scraf {database, endpoint, 1};
+	std::thread servingThread {[&]() {
+		scraf.serve();
+	}};
+	EXPECT_EQ(
+		cpr::Post(
+			cpr::Url{"localhost:" + std::to_string(port) + "/students"},
+			cpr::Header{{"Content-Type", "application/json"}},
+			cpr::Body{nlohmann::json{
+				{"mail", "luca.lettini@libero.it"},
+				{"password", "tuamammma68"},
+				{"name", "Luca"}
+			
+			}.dump()}
+		).status_code, 
+		static_cast<std::int32_t>(Http::Code::Created)); //la richiesta effettuata senza il cognome dovrebbe riuscire perchè il cognome è opzionale
+	scraf.shutdown();
+	servingThread.join();
+}
+
+//test in cui la richiesta è effettuata senza l'email = dovrebbe non funzionare
+TEST(ScrafTest, PasswordNameSurnameWhithoutEmail) {
+	constexpr std::uint16_t port {10780};
+	std::unique_ptr<FakeDatabase> database {std::make_unique<FakeDatabase>()};
+	Http::Endpoint endpoint{{Ipv4::loopback(), Port(port)}};
+	Scraf<std::unique_ptr<FakeDatabase>, FakeDbTransaction> scraf {database, endpoint, 1};
+	std::thread servingThread {[&]() {
+		scraf.serve();
+	}};
+	EXPECT_EQ(
+		cpr::Post(
+			cpr::Url{"localhost:" + std::to_string(port) + "/students"},
+			cpr::Header{{"Content-Type", "application/json"}},
+			cpr::Body{nlohmann::json{
+				{"password", "ciaociao22"},
+				{"name", "Luca"},
+				{"surname", "Lettini"}
+			}.dump()}
+		).status_code, 
+		static_cast<std::int32_t>(Http::Code::Not_Acceptable)); //la richiesta effettuata senza l'email dovrebbe dare uno status code che indica che la richiesta è incompleta
+	scraf.shutdown();											//lo status code corrispondente al NOT_ACCEPTABLE è il 406
+	servingThread.join();
+}
+
+//test in cui la richiesta è effettuata senza la password = dovrebbe non funzionare
+TEST(ScrafTest, MailNameSurnameWhithoutPassword) {
+	constexpr std::uint16_t port {10780};
+	std::unique_ptr<FakeDatabase> database {std::make_unique<FakeDatabase>()};
+	Http::Endpoint endpoint{{Ipv4::loopback(), Port(port)}};
+	Scraf<std::unique_ptr<FakeDatabase>, FakeDbTransaction> scraf {database, endpoint, 1};
+	std::thread servingThread {[&]() {
+		scraf.serve();
+	}};
+	EXPECT_EQ(
+		cpr::Post(
+			cpr::Url{"localhost:" + std::to_string(port) + "/students"},
+			cpr::Header{{"Content-Type", "application/json"}},
+			cpr::Body{nlohmann::json{
+				{"mail", "lettilu22@gmail.com"},
+				{"name", "Marco"},
+				{"surname", "IlSupremo"}
+			}.dump()}
+		).status_code, 
+		static_cast<std::int32_t>(Http::Code::Not_Acceptable)); //la richiesta effettuata senza la password dovrebbe dare uno status code che indica che la richiesta è incompleta
+	scraf.shutdown();											//lo status code corrispondente al NOT_ACCEPTABLE è il 406
+	servingThread.join();
+}
+
+//test in cui la richiesta è effettuata senza il nome = dovrebbe non funzionare
+TEST(ScrafTest, MailPasswordSurnameWhithoutName) {
+	constexpr std::uint16_t port {10780};
+	std::unique_ptr<FakeDatabase> database {std::make_unique<FakeDatabase>()};
+	Http::Endpoint endpoint{{Ipv4::loopback(), Port(port)}};
+	Scraf<std::unique_ptr<FakeDatabase>, FakeDbTransaction> scraf {database, endpoint, 1};
+	std::thread servingThread {[&]() {
+		scraf.serve();
+	}};
+	EXPECT_EQ(
+		cpr::Post(
+			cpr::Url{"localhost:" + std::to_string(port) + "/students"},
+			cpr::Header{{"Content-Type", "application/json"}},
+			cpr::Body{nlohmann::json{
+				{"mail", "lucalettini54@gmail.com"},
+				{"password", "marcopolo22"},
+				{"surname", "IlSupremo"}
+			}.dump()}
+		).status_code, 
+		static_cast<std::int32_t>(Http::Code::Not_Acceptable)); //la richiesta effettuata senza il nome dovrebbe dare uno status code che indica che la richiesta è incompleta
+	scraf.shutdown();											//lo status code corrispondente al NOT_ACCEPTABLE è il 406
+	servingThread.join();
+}
